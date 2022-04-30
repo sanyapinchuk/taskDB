124224fa7913097043928d00345f6eb2901b12f8
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2022-04-30 19:32:51 +0300

    change tables

--- task.sql
+++ task.sql
@@ -1,14 +1,27 @@
-CREATE DATABASE taskDB;
-USE taskDB;
+--CREATE DATABASE taskDB;
+USE task1DB;
 
 CREATE TABLE Banks (
 	idBank INT PRIMARY KEY NOT NULL IDENTITY,
-    nameBank INT
+    nameBank nvarchar(20)
 );
 
 CREATE TABLE Branch (
 	idBranch INT PRIMARY KEY NOT NULL IDENTITY,
-    nameBank INT
+    idBank INT,
+	idTower INT, 
+	idStreet INT,
+	idHouse INT,
+);
+
+CREATE TABLE Street (
+	idStreet INT PRIMARY KEY NOT NULL IDENTITY,
+	nameStreet nvarchar(20)
+);
+
+CREATE TABLE House (
+	idHouse INT PRIMARY KEY NOT NULL IDENTITY,
+	number INT
 );
 
 CREATE TABLE Tower (
@@ -20,7 +33,7 @@ CREATE TABLE Account (
 	idAccount INT PRIMARY KEY NOT NULL IDENTITY,
     idClient INT,
     idBank INT,
-    cash decimal(19,4)
+    cash money
     );
     
 CREATE TABLE Client (
@@ -30,15 +43,15 @@ CREATE TABLE Client (
     idStatus INT
 );
 
-CREATE TABLE Status (
+CREATE TABLE SocStatus (
 	idStatus INT PRIMARY KEY IDENTITY,
     nameStatus nvarchar(15)
 );
 
-CREATE TABLE Card (
+CREATE TABLE Cards (
 	idCard INT PRIMARY KEY IDENTITY,
     serialNumber nvarchar(16),
-    cash decimal(19,4),
+    cash money,
     idAccount INT
 );
     
